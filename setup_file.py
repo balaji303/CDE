607c2c5793fc50745c18b9c89d86d5bbdd0ae8b0
balaji303 <balaji303@outlook.com>
2021-09-04 18:45:26 +0530

    v2 added

--- setup_file.py
+++ setup_file.py
@@ -17,26 +17,33 @@ class GUI ( wx.Frame ):
         wx.Frame.__init__(self, 
                           None, 
                           -1, 
-                          u"CD-E Your C Language IDE v2", 
+                          u"CD-E Your Language IDE v2", 
                           size=(500, 500),#Window size
                           style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
         
         
         self.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
-
         self.SetBackgroundColour( wx.Colour( 182, 180, 235 ) )
+        
+        langlist = ['C', 'C++'] 
         self.text1 = wx.StaticText( self, 
                                     -1,
-                                    "Enter the C Code:", 
-                                    ( 40,30 ), 
+                                    "Enter the Code:", 
+                                    ( 40,50 ), 
                                     style=wx.TE_RICH ) 
         self.text1.SetForegroundColour( wx.BLACK )
-     
+        self.languagebox = wx.RadioBox(self, 
+                                label = 'Language',
+                                pos = (200,5), 
+                                choices = langlist,
+                                majorDimension = 1,
+                                style = wx.RA_SPECIFY_ROWS)
+
         self.text = wx.richtext.RichTextCtrl(self, 
                                               wx.ID_ANY, 
                                               wx.EmptyString, 
-                                              wx.Point( 40,50 ), 
-                                              wx.Size( 400,350 ), 
+                                              wx.Point( 40,70 ), 
+                                              wx.Size( 400,340 ), 
                                               0|wx.VSCROLL|
                                               wx.HSCROLL|wx.NO_BORDER|
                                               wx.WANTS_CHARS|wx.FONTSIZE_X_LARGE )
@@ -44,7 +51,7 @@ class GUI ( wx.Frame ):
         self.compile_button = wx.Button(self, 
                                    wx.ID_ANY, 
                                    u"Run", 
-                                   wx.Point(210,420))
+                                   wx.Point(210,425))
         self.compile_button.Bind(wx.EVT_BUTTON, self.compile_fun) 
 
 
@@ -52,10 +59,19 @@ class GUI ( wx.Frame ):
         code = self.text.GetValue()        
         pyfiledir  = os.path.dirname( __file__ )
         os.chdir(pyfiledir)
-        with open( "setup.c","w" ) as cfile:
-            cfile.write(code)
-        cfile.close()
-        os.system("gcc setup.c -o setup")
+        language=self.languagebox.GetStringSelection()
+        if language == 'C':
+            with open( "setup.c","w" ) as cfile:
+                cfile.write(code)
+            cfile.close()
+            os.system("gcc setup.c -o setup")
+        elif language == 'C++':
+            with open( "setup.cpp","w" ) as cppfile:
+                cppfile.write(code)
+            cppfile.close()
+            os.system("g++ setup.cpp -o setup")
+        else:
+            print("Error")
         os.system(".\setup")
         return
         
