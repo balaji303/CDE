c05b9492638566aef7efdf25193b217b7a1392b4
balaji303 <balaji303@outlook.com>
2021-09-11 06:44:52 +0530

    Flask8 bugs

--- CDE/CDE.py
+++ CDE/CDE.py
@@ -1,45 +1,33 @@
-#CD-E
-
-
+# CD-E
 import wx
-from wx.core import Size
 import wx.xrc
 import wx.aui
 import wx.richtext
 import os
-
-
 class GUI ( wx.Frame ):
-    
     def __init__( self, parent ):
-    
-        
-        wx.Frame.__init__(self, 
-                          None, 
-                          -1, 
-                          u"CD-E Your Language IDE v2", 
-                          size=(500, 500),#Window size
-                          style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
-        
-        
+        wx.Frame.__init__( self, 
+                           None, 
+                           -1, 
+                           u"CD-E Your Language IDE v2", 
+                           size=( 500, 500 ),#Window size
+                           style = wx.DEFAULT_FRAME_STYLE & ~( wx.RESIZE_BORDER | wx.MAXIMIZE_BOX ) )
         self.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
         self.SetBackgroundColour( wx.Colour( 182, 180, 235 ) )
-        
-        langlist = ['C', 'C++'] 
+        langlist = [ 'C' , 'C++' ] 
         self.text1 = wx.StaticText( self, 
                                     -1,
                                     "Enter the Code:", 
                                     ( 40,50 ), 
                                     style=wx.TE_RICH ) 
         self.text1.SetForegroundColour( wx.BLACK )
-        self.languagebox = wx.RadioBox(self, 
+        self.languagebox = wx.RadioBox( self, 
                                 label = 'Language',
-                                pos = (200,5), 
+                                pos = ( 200,5 ), 
                                 choices = langlist,
                                 majorDimension = 1,
                                 style = wx.RA_SPECIFY_ROWS)
-
-        self.text = wx.richtext.RichTextCtrl(self, 
+        self.text = wx.richtext.RichTextCtrl( self, 
                                               wx.ID_ANY, 
                                               wx.EmptyString, 
                                               wx.Point( 40,70 ), 
@@ -47,14 +35,11 @@ class GUI ( wx.Frame ):
                                               0|wx.VSCROLL|
                                               wx.HSCROLL|wx.NO_BORDER|
                                               wx.WANTS_CHARS|wx.FONTSIZE_X_LARGE )
-        
         self.compile_button = wx.Button(self, 
                                    wx.ID_ANY, 
                                    u"Run", 
                                    wx.Point(210,425))
         self.compile_button.Bind(wx.EVT_BUTTON, self.compile_fun) 
-
-
     def compile_fun(self,e):
         code = self.text.GetValue()        
         pyfiledir  = os.path.dirname( __file__ )
@@ -74,10 +59,7 @@ class GUI ( wx.Frame ):
             print("Error")
         os.system(".\program")
         return
-        
-
 if __name__ == '__main__':
-    
     app = wx.App(False) 
     app_gui = GUI(None) 
     app_gui.Show(True)      
