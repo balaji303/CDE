44b099a4e8b1471e82dde1af8c0bef7f45df65dc
balaji303 <balaji303@outlook.com>
2021-09-11 06:49:19 +0530

    Flask8 bugs2

--- CDE/CDE.py
+++ CDE/CDE.py
@@ -4,37 +4,37 @@ import wx.xrc
 import wx.aui
 import wx.richtext
 import os
-class GUI ( wx.Frame ):
-    def __init__( self, parent ):
-        wx.Frame.__init__( self, 
-                           None, 
-                           -1, 
-                           u"CD-E Your Language IDE v2", 
-                           size=( 500, 500 ),#Window size
-                           style = wx.DEFAULT_FRAME_STYLE & ~( wx.RESIZE_BORDER | wx.MAXIMIZE_BOX ) )
-        self.SetForegroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
-        self.SetBackgroundColour( wx.Colour( 182, 180, 235 ) )
-        langlist = [ 'C' , 'C++' ] 
-        self.text1 = wx.StaticText( self, 
-                                    -1,
-                                    "Enter the Code:", 
-                                    ( 40,50 ), 
-                                    style=wx.TE_RICH ) 
-        self.text1.SetForegroundColour( wx.BLACK )
-        self.languagebox = wx.RadioBox( self, 
+class GUI (wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, 
+                          None, 
+                          -1, 
+                          u"CD-E Your Language IDE v2", 
+                          size=(500, 500),#Window size
+                          style = wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER | wx.MAXIMIZE_BOX))
+        self.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
+        self.SetBackgroundColour(wx.Colour(182, 180, 235))
+        langlist = ['C' , 'C++'] 
+        self.text1 = wx.StaticText(self, 
+                                   -1,
+                                   "Enter the Code:", 
+                                   (40,50), 
+                                    style=wx.TE_RICH) 
+        self.text1.SetForegroundColour(wx.BLACK)
+        self.languagebox = wx.RadioBox(self, 
                                 label = 'Language',
-                                pos = ( 200,5 ), 
+                                pos = (200,5), 
                                 choices = langlist,
                                 majorDimension = 1,
                                 style = wx.RA_SPECIFY_ROWS)
-        self.text = wx.richtext.RichTextCtrl( self, 
-                                              wx.ID_ANY, 
-                                              wx.EmptyString, 
-                                              wx.Point( 40,70 ), 
-                                              wx.Size( 400,340 ), 
-                                              0|wx.VSCROLL|
-                                              wx.HSCROLL|wx.NO_BORDER|
-                                              wx.WANTS_CHARS|wx.FONTSIZE_X_LARGE )
+        self.text = wx.richtext.RichTextCtrl(self, 
+                                             wx.ID_ANY, 
+                                             wx.EmptyString, 
+                                             wx.Point(40,70), 
+                                             wx.Size(400,340), 
+                                             0|wx.VSCROLL|
+                                             wx.HSCROLL|wx.NO_BORDER|
+                                             wx.WANTS_CHARS|wx.FONTSIZE_X_LARGE)
         self.compile_button = wx.Button(self, 
                                    wx.ID_ANY, 
                                    u"Run", 
